44dec1c36376f7083a338b48fb721428e114d277
ajhernandez95 <31139761+ajhernandez95@users.noreply.github.com>
2018-08-09 21:50:55 -0500

    Set up app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="drum-machine">
+      
+      </div>
+    );
+  }
+}
+
+export default App;
