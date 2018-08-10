f2c8b222867b18342acb30f607dcdd6ec276bf2b
ajhernandez95 <31139761+ajhernandez95@users.noreply.github.com>
2018-08-10 16:49:16 -0500

    more set up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div id="drum-machine">
-     
+      <div class='pad-wrap'>
       <div class='row-one'>
       <div class="drum-pad" id=""><h1>Q</h1></div>
       <div class="drum-pad" id=""><h1>W</h1></div>
@@ -22,8 +22,12 @@ class App extends Component {
       <div class="drum-pad" id=""><h1>C</h1></div>
       </div>
       </div>
-     
-    );
+      <div class='ctrl-wrap'>
+      <h1>ctrl-wrap</h1>
+      <div class='switch' />
+      </div>
+      </div>
+         );
   }
 }
 
