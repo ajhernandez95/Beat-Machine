81cd6ac6b81be28c8d77ed1cb120b35fa5ba6fce
ajhernandez95 <31139761+ajhernandez95@users.noreply.github.com>
2018-08-10 12:59:05 -0500

    Almost finished pad layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div id="drum-machine">
-      
+     
+      <div class='row-one'>
+      <div class="drum-pad" id=""><h1>Q</h1></div>
+      <div class="drum-pad" id=""><h1>W</h1></div>
+      <div class="drum-pad" id=""><h1>E</h1></div>
       </div>
+      <div class='row-two'>
+      <div class="drum-pad" id=""><h1>A</h1></div>
+      <div class="drum-pad" id=""><h1>S</h1></div>
+      <div class="drum-pad" id=""><h1>D</h1></div>
+      </div>
+      <div class='row-three'>
+      <div class="drum-pad" id=""><h1>Z</h1></div>
+      <div class="drum-pad" id=""><h1>X</h1></div>
+      <div class="drum-pad" id=""><h1>C</h1></div>
+      </div>
+      </div>
+     
     );
   }
 }
