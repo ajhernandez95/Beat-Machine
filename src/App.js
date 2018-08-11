ff126c54db81d5ae8da6bd70e4ae1ae5e2af175c
ajhernandez95 <31139761+ajhernandez95@users.noreply.github.com>
2018-08-10 21:23:29 -0500

    html for ctrlpad
    still need css

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Ctrlpad from './Components/ctrlpad'
+import Beatpad from './Components/beatpad.js';
 
 class App extends Component {
   render() {
     return (
       <div id="drum-machine">
-      <div class='pad-wrap'>
-      <div class='row-one'>
-      <div class="drum-pad" id=""><h1>Q</h1></div>
-      <div class="drum-pad" id=""><h1>W</h1></div>
-      <div class="drum-pad" id=""><h1>E</h1></div>
+        <Beatpad />
+        <Ctrlpad />
+        
       </div>
-      <div class='row-two'>
-      <div class="drum-pad" id=""><h1>A</h1></div>
-      <div class="drum-pad" id=""><h1>S</h1></div>
-      <div class="drum-pad" id=""><h1>D</h1></div>
-      </div>
-      <div class='row-three'>
-      <div class="drum-pad" id=""><h1>Z</h1></div>
-      <div class="drum-pad" id=""><h1>X</h1></div>
-      <div class="drum-pad" id=""><h1>C</h1></div>
-      </div>
-      </div>
-      <div class='ctrl-wrap'>
-      <h1>ctrl-wrap</h1>
-      <div class='switch' />
-      </div>
-      </div>
-         );
+    );
   }
 }
 
