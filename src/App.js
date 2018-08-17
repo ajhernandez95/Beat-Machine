b718b791ae61920c7167c43f46ee58066ef08974
ajhernandez95 <31139761+ajhernandez95@users.noreply.github.com>
2018-08-17 13:03:20 -0500

    once again restarted
    but much better this time

--- src/App.js
+++ src/App.js
@@ -1,67 +1,94 @@
 import React, { Component } from "react";
 import "./App.css";
 
-//let audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3')
+const bank = [{
+  keyCode: 81,
+  letter: 'Q',
+  id: 'Heater-1',
+  src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+}, {
+  keyCode: 87,
+  letter: 'W',
+  id: 'Heater-2',
+  src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+}, {
+  keyCode: 69,
+  letter: 'E',
+  id: 'Heater-3',
+  src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+}, {
+  keyCode: 65,
+  letter: 'A',
+  id: 'Heater-4',
+  src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+}, {
+  keyCode: 83,
+  letter: 'S',
+  id: 'Clap',
+  src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+}, {
+  keyCode: 68,
+  letter: 'D',
+  id: 'Open-HH',
+  src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+}, {
+  keyCode: 90,
+  letter: 'Z',
+  id: "Kick-n'-Hat",
+  src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+}, {
+  keyCode: 88,
+  letter: 'X',
+  id: 'Kick',
+  src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+}, {
+  keyCode: 67,
+  letter: 'C',
+  id: 'Closed-HH',
+  src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+},
+];
+
+class Drumpad extends Component{
+
+  handleClick = () => {
+    this.audio.play()
+    this.audio.curretnTime = 0
+  }
+
+  render() {
+    return (
+      <div className='drum-pad' id='this.props.id' onClick={this.handleClick}>
+      <p>{this.props.letter}</p>
+      <audio ref={ref => this.audio = ref} id={this.props.id} src={this.props.src}></audio>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.onClick = this.onClick.bind(this)
-   
-  }
 
-
-  onClick(e) {
-    let itemId = JSON.stringify(e.target.getAttribute('id'))
-    document.getElementById(itemId).style.backgroundColor = 'blue'
-    console.log(itemId)
-    
-    
   }
-  render() {
-    return (
-      <div id="drum-machine">
-        <div id="display">
-          <div class="drum-pad-wrapper">
-            <div class="drum-pad" id="test" onClick={this.onClick}>
-              <audio 
-                id="sound"
-                src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-                preload
-              />
-              Q
-            </div>
-            <div class="drum-pad" id="test2" onClick={this.onClick}>
-              W
-            </div>
-            <div class="drum-pad" id="test3" onClick={this.onClick}>
-              E
-            </div>
-            <div class="drum-pad" id="test4" onClick={this.onClick}>
-              A
-            </div>
-            <div class="drum-pad" id="test5" onClick={this.onClick}>
-              S
-            </div>
-            <div class="drum-pad" id="test6" onClick={this.onClick}>
-              D
-            </div>
-            <div class="drum-pad" id="test7" onClick={this.onClick}>
-              Z
-            </div>
-            <div class="drum-pad" id="test8" onClick={this.onClick}>
-              X
-            </div>
-            <div class="drum-pad" id="test9" onClick={this.onClick}>
-              C
-            </div>
-          </div>
-        </div>
+render() {
+  return (
+    <div id="drum-machine">
+      <div id='display'>
+        {bank.map(i => (
+          <Drumpad 
+          id={i.id}
+          letter={i.letter}
+          src={i.src}
+          />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
+}
+
 
 export default App;
 //make beat sound's div id's and p id's 
